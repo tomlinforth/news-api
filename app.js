e4aeda7ed7d27ca4458341869dc1168724cadb38
Tom Linforth <tom.linforth1@gmail.com>
2020-01-17 08:48:38 +0000

    error handling seperated out

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const apiRouter = require("./routers/api-router");
+const { psqlErr, customErr } = require("./errors/index");
 
 const app = express();
 
@@ -10,22 +11,8 @@ app.get("/*", function notARoute(req, res, next) {
   res.status(404).send({ msg: "Requested URL doesnt exist." });
 });
 
-app.use(function psqlErr(err, req, res, next) {
-  if (err.code) {
-    const errRef = {
-      "22P02": { status: 400, msg: "Invalid data type (integer needed)." },
-      "22003": { status: 400, msg: "Invalid ID (number too long)." },
-      "23503": { status: 404, msg: "Specified ID does not exist." },
-      "23502": { status: 400, msg: "Missing data in request." }
-    };
-    res.status(errRef[err.code].status).send({ msg: errRef[err.code].msg });
-  } else {
-    next(err);
-  }
-});
+app.use(psqlErr);
 
-app.use(function customErr(err, req, res, next) {
-  res.status(err.status).send({ msg: err.msg });
-});
+app.use(customErr);
 
 module.exports = app;
