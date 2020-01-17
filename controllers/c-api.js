ccab8506d739ec9b5c2594bbf50034f3600d9fda
Tom Linforth <tom.linforth1@gmail.com>
2020-01-17 09:24:27 +0000

    removed unnecessary logs

--- controllers/c-api.js
+++ controllers/c-api.js
@@ -2,7 +2,6 @@ exports.sendAllEndpoints = (req, res, next) => {
   const options = {
     root: "./"
   };
-  console.log(options);
   res.sendFile("endpoints.json", options, err => {
     if (err) next(err);
   });
